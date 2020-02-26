d54dcf4ebfcefddcb0abfef021f43b5326d538e8
Raul Silas Lima de Oliveira <raul.oliveira@ax4b.com>
2020-02-25 21:51:26 -0300

    Segundo push

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,14 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
+router.get('/', (req, res, next) => {
+    res.status(200).send({ 
+        tittle: "Node Store API", 
+        version: "0.0.2",
+        name: "Dev+"
+    });
+});
+
 routes.get('/products', ProductController.index);
 routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
